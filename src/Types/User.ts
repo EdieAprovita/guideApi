7b5e6fbafa73f5da813265d276c1d5af50101496
Edgar Chaver <edieveg316@gmail.com>
2023-05-02 16:00:43 -0600

    Dev Branch
    Create a dev branch and types

---
+++ src/Types/User.ts
@@ -0,0 +1,183 @@
+import { Types } from "mongoose";
+
+enum Role {
+	ADMIN = "ADMIN",
+	USER = "USER",
+	PROFESSIONAL = "PROFESSIONAL",
+}
+
+enum Budget {
+	LOW = "LOW",
+	MEDIUM = "MEDIUM",
+	HIGH = "HIGH",
+}
+
+enum TypeDish {
+	"BREAKFAST" = "BREAKFAST",
+	"LUNCH" = "LUNCH",
+	"DINNER" = "DINNER",
+	"SNACK" = "SNACK",
+	"DESSERT" = "DESSERT",
+}
+
+enum TypePlace {
+	"RESTAURANT" = "RESTAURANT",
+	"STREETFOOD" = "STREETFOOD",
+}
+
+enum Difficulty {
+	"EASY" = "EASY",
+	"MEDIUM" = "MEDIUM",
+	"HARD" = "HARD",
+}
+
+export interface IUser {
+	username: string;
+	password: string;
+	email: string;
+	role: Role;
+	photo: string;
+	timestamps: Date;
+}
+
+export interface IReview {
+	_id?: string;
+	username: string;
+	rating: number;
+	comment: string;
+	user: Types.ObjectId;
+	timestamps: {
+		createdAt: Date;
+		updatedAt: Date;
+	};
+}
+
+export interface IBusiness {
+	_id?: string;
+	namePlace: string;
+	author: Types.ObjectId;
+	address: string;
+	image: string;
+	constact: [
+		{
+			phone: string;
+			facebook?: string;
+			instagram?: string;
+			email?: string;
+		}
+	];
+	budget: Budget;
+	reviews: IReview[];
+	rating: number;
+	timestamps: {
+		createdAt: Date;
+		updatedAt: Date;
+	};
+}
+
+export interface IMedic {
+	_id?: string;
+	doctorName: string;
+	author: Types.ObjectId;
+	address: string;
+	image: string;
+	speciality: string;
+	constact: [
+		{
+			phone: string;
+			facebook?: string;
+			instagram?: string;
+			email: string;
+		}
+	];
+	reviews: IReview[];
+	rating: number;
+	numReviews: number;
+	timestamps: {
+		createdAt: Date;
+		updatedAt: Date;
+	};
+}
+
+export interface IMarket {
+	_id?: string;
+	marketName: string;
+	author: Types.ObjectId;
+	address: string;
+	image: string;
+	typeMarket: string;
+	reviews: [IReview];
+	rating: number;
+	numReviews: number;
+	timestamps: {
+		createdAt: Date;
+		updatedAt: Date;
+	};
+}
+
+export interface IProfession {
+	_id?: string;
+	professionName: string;
+	author: Types.ObjectId;
+	specialty: string;
+	contact: [
+		{
+			phone: number;
+			email: string;
+			facebook?: string;
+			instagram?: string;
+		}
+	];
+	reviews: [IReview];
+	rating: number;
+	numReviews: number;
+	timestamps: {
+		createdAt: Date;
+		updatedAt: Date;
+	};
+}
+
+export interface IRecipe {
+	_id?: string;
+	recipeName: string;
+	author: Types.ObjectId;
+	description: string;
+	instructions: string;
+	ingredients: [string];
+	typeDish: TypeDish;
+	image: string;
+	cookingTime: number;
+	difficulty: Difficulty;
+	reviews: [IReview];
+	rating: number;
+	numReviews: number;
+	budget: string;
+	timestamps: {
+		createdAt: Date;
+		updatedAt: Date;
+	};
+}
+
+export interface IRestaurant {
+	_id?: string;
+	restaurantName: string;
+	author: Types.ObjectId;
+	typePlace: TypePlace;
+	address: string;
+	image: string;
+	budget: Budget;
+	contact: [
+		{
+			phone: number;
+			facebook: string;
+			instagram: string;
+		}
+	];
+	reviews: [IReview];
+	rating: number;
+	numReviews: number;
+	timestamps: {
+		createdAt: Date;
+		updatedAt: Date;
+	};
+}
